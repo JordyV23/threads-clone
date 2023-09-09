a2b2320971aef8c7f2da26314ae3b2c09474134e
JordyV23 <jordyvargas0123@gmail.com>
2023-09-08 19:53:45 -0600

    Configuracion inicial

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  //Habilita el router
+  pages: true,
+  //Carga los modulos de nuxt
+  modules: [
+    "nuxt-icon",
+    "@nuxtjs/tailwindcss",
+    // "@nuxtjs/supabase",
+    "@pinia/nuxt",
+    "@vite-pwa/nuxt",
+  ],
+  //Configuracion de la app que no debe de ser expuesta
+  //En el frontend
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URLS,
+    },
+  },
   devtools: { enabled: true },
 });
